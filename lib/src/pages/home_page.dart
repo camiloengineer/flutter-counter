5c292dd502729a4e4fe8e42c90aa3856d43387ba
Camilo González <cgonzalezm1234@gmail.com>
2021-07-28 22:07:50 -0400

    first commit

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final estiloTexto = TextStyle(fontSize: 40);
+
+  final conteo = 10;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Título'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Número de clicks:', style: estiloTexto),
+            Text('$conteo', style: estiloTexto),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          print('Hola mundo');
+          //conteo = conteo + 1;
+        },
+      ),
+    );
+  }
+}
